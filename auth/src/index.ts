141926b6dd1add10047b822d4df9be3f446dec47
M0nster92 <fahimalsafi@gmail.com>
2023-05-19 20:54:10 -0600

    added skaffold and ingress

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,8 +1,12 @@
-import express from "express";
+import express, { Request, Response } from "express";
 
 const app = express();
 app.use(express.json());
 
+app.get("/api/users/currentuser", (req: Request, res: Response) => {
+  res.send("Users!");
+});
+
 app.listen(3000, () => {
-  console.log("Auth service listening on port 3000");
+  console.log("auth service listening on port 3000");
 });
