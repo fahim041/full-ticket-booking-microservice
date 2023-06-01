ff8c4de8b0e54e1b49fe76f29f6ac6be5b2ac1aa
M0nster92 <fahimalsafi@gmail.com>
2023-05-31 23:16:35 -0400

    fully authenticated service

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,19 +1,14 @@
-import express from "express";
-import jwt from "jsonwebtoken";
+import express, { Request, Response } from "express";
+import { currentUser } from "../middleware/current-user";
 
 const router = express.Router();
 
-router.get("/api/users/currentuser", (req, res) => {
-  if (!req.session?.jwt) {
-    return res.send({ currentUser: null });
+router.get(
+  "/api/users/currentuser",
+  currentUser,
+  (req: Request, res: Response) => {
+    res.send({ currentUser: req.currentUser || null });
   }
-
-  try {
-    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
-    res.send({ currentUser: payload });
-  } catch (err) {
-    return res.send({ currentUser: null });
-  }
-});
+);
 
 export { router as currentUserRouter };
