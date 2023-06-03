05bb1e7614b049ae6ce8d1d682a15ee9d6ceb344
M0nster92 <fahimalsafi@gmail.com>
2023-06-02 21:51:34 -0400

    app config refactor & test env setup & sign up route tests

---
+++ auth/src/app.ts
@@ -0,0 +1,36 @@
+import express from 'express';
+import 'express-async-errors';
+import cookieSession from 'cookie-session';
+import { currentUserRouter } from './routes/current-user';
+import { signInRouter } from './routes/signin';
+import { signOutRouter } from './routes/signout';
+import { signupRouter } from './routes/signup';
+import { errorHandler } from './middleware/error-handler';
+import { NotFoundError } from './errors/not-found-error';
+
+const app = express();
+app.set('trust proxy', true);
+app.use(express.json());
+
+app.use(
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== 'test',
+  })
+);
+
+//routes
+app.use(currentUserRouter);
+app.use(signInRouter);
+app.use(signOutRouter);
+app.use(signupRouter);
+
+//not found routes
+app.all('*', async (req, res, next) => {
+  throw new NotFoundError();
+});
+
+//middlwares
+app.use(errorHandler);
+
+export { app };
--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,39 +1,6 @@
-import express from "express";
-import "express-async-errors";
 import mongoose from "mongoose";
-import cookieSession from "cookie-session";
-import { currentUserRouter } from "./routes/current-user";
-import { signInRouter } from "./routes/signin";
-import { signOutRouter } from "./routes/signout";
-import { signupRouter } from "./routes/signup";
-import { errorHandler } from "./middleware/error-handler";
-import { NotFoundError } from "./errors/not-found-error";
 import { DatabaseConnectionError } from "./errors/database-connection-error";
-
-const app = express();
-app.set("trust proxy", true);
-app.use(express.json());
-
-app.use(
-  cookieSession({
-    signed: false,
-    secure: true,
-  })
-);
-
-//routes
-app.use(currentUserRouter);
-app.use(signInRouter);
-app.use(signOutRouter);
-app.use(signupRouter);
-
-//not found routes
-app.all("*", async (req, res, next) => {
-  throw new NotFoundError();
-});
-
-//middlwares
-app.use(errorHandler);
+import { app } from "./app";
 
 const start = async () => {
   if (!process.env.JWT_KEY) {
