f313327b99cd9a46667c8561b7c3932049ce065f
M0nster92 <fahimalsafi@gmail.com>
2023-06-02 22:08:16 -0400

    fixed sigout bug & test cases

--- auth/src/routes/signout.ts
+++ auth/src/routes/signout.ts
@@ -1,9 +1,9 @@
-import express from "express";
-import cookieSession from "cookie-session";
+import express from 'express';
+import cookieSession from 'cookie-session';
 
 const router = express.Router();
 
-router.get("/api/users/signout", (req, res) => {
+router.post('/api/users/signout', (req, res) => {
   req.session = null;
   res.send({});
 });
