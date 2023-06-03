0f5cd0ac565874981912b33b5bef09972da9792e
M0nster92 <fahimalsafi@gmail.com>
2023-06-02 22:52:30 -0400

    auth service is completed

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -1,9 +1,13 @@
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from 'mongoose';
+import request from 'supertest';
 import { app } from '../app';
 
-let mongo: any;
+declare global {
+  function signin(): Promise<string[]>;
+}
 
+let mongo: any;
 beforeAll(async () => {
   //dependency variable to start the server
   process.env.JWT_KEY = 'asdf';
@@ -25,3 +29,20 @@ afterAll(async () => {
   await mongoose.connection.close();
   await mongo.stop();
 });
+
+global.signin = async () => {
+  const email = 'test@test.com';
+  const password = 'password';
+
+  const res = await request(app)
+    .post('/api/users/signup')
+    .send({
+      email,
+      password,
+    })
+    .expect(201);
+
+  const cookie = res.get('Set-Cookie');
+
+  return cookie;
+};
